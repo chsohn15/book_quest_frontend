71aa75fc864ec68da4d4949804565545960cc882
chsohn15 <chsohn15@gmail.com>
2020-10-08 13:01:37 -0400

    tried useEffect

--- book-quest-react/src/components/bookSearch/Test.js
+++ book-quest-react/src/components/bookSearch/Test.js
@@ -12,25 +12,24 @@ const Test = () => {
         const script1 = document.createElement('script')
         script1.type = "text/javascript"
         script1.async = true 
-        script1.src ="https://www.google.com/books/jsapi.js"
+        
 
         // const script2 = document.createElement('script')
         // script2.type = "text/javascript"
         // script2.async = true 
 
-        //  script2.innerHTML = `
-        // google.books.load();
+         script1.innerHTML = `
 
-        // function initialize() {
-        //     var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
-        //     viewer.load('ISBN:0738531367');
-        //   }
+        function initialize() {
+            var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+            viewer.load('ISBN:0738531367');
+          }
 
-        //   google.books.setOnLoadCallback(initialize)`
+          google.books.setOnLoadCallback(initialize)`
 
           document.body.appendChild(script1)
         //   document.body.appendChild(script2)
-        debugger
+
         return() => {
             document.body.removeChild(script1)
             // document.body.removeChild(script2)
