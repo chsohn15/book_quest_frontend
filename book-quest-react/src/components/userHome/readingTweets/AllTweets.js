876f9d967238304f72c39dc6b334c07055063d09
chsohn15 <chsohn15@gmail.com>
2020-10-14 16:33:12 -0400

    add loadingAllTweets action function

--- book-quest-react/src/components/userHome/readingTweets/AllTweets.js
+++ book-quest-react/src/components/userHome/readingTweets/AllTweets.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { loadingAllTweets } from '../../../redux/actions.js'
 
 const AllTweets = (props) => {
 
-    
+    useEffect(() => {
+        props.loadingAllTweets()
+    }, [])
+
     return(
     <div>
         <div>List of All Tweets by a User</div>
@@ -11,4 +17,9 @@ const AllTweets = (props) => {
     )
 }
 
-export default AllTweets
+const mapDispatchToProps = dispatch => {
+    return {
+        loadingAllTweets: (() => {{ dispatch( loadingAllTweets() )}
+    })}}
+
+export default connect(null, mapDispatchToProps)(AllTweets)
--- book-quest-react/src/redux/actions.js
+++ book-quest-react/src/redux/actions.js
@@ -12,7 +12,27 @@ const TWEET_DATA_URL = "http://localhost:3000/api/v1/get_tweet_data"
 const VOCAB_URL = "http://localhost:3000/api/v1/vocab_activities"
 const RESTFUL_TWEET_URL = "http://localhost:3000/api/v1/reading_tweets/"
 const GET_VOCAB = "http://localhost:3000/api/v1/get_vocab"
+const GET_ALL_TWEETS = "http://localhost:3000/api/v1/get_all_tweets"
 
+function loadedAllTweets(){
+
+}
+
+function loadingAllTweets(){
+    return (dispatch) => {
+        fetch(GET_ALL_TWEETS, {method: "POST", 
+        headers: {
+            "Content-Type":"application/json",
+            Authorization: `Bearer ${localStorage.token}`
+        },
+        body: JSON.stringify({
+            id: localStorage.user_id
+        })})
+        .then(res => res.json()) 
+        .then(allTweets => console.log(allTweets))
+        // .then(allTweets => dispatch(loadedAllTweets(allTweets)))
+    }
+}
 
 function submittedVocab(words){
     return {type: "ADD_VOCAB_WORD", payload: words}
@@ -306,4 +326,5 @@ export {
     loadingUser,
     loadingTweetData,
     deletingTweet, 
-    submittingVocab}
+    submittingVocab,
+    loadingAllTweets}
