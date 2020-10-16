fd7919f38b6172130183188ae03a867a88e720ca
chsohn15 <chsohn15@gmail.com>
2020-10-16 11:43:23 -0400

    added comments to bookviewer component

--- book-quest-react/src/components/userHome/books/BookViewer.js
+++ book-quest-react/src/components/userHome/books/BookViewer.js
@@ -8,11 +8,30 @@ const BookViewer = () => {
     const ISBN_num = useSelector(state => state.currentBookReducer.currentBook.book.ISBN_number)
     const canvasRef = useRef()
 
+    //Make state of loading as true
+    //When component is mounted, set state of loading to false
+
     useEffect(() => {
         console.log(canvasRef)
 
         //Check if google.books.load is a function
-        window.google.books.load()
+
+        // if (window.google.books){
+        // window.google.books.load() 
+        // window.google.books.setOnLoadCallback(initialize)
+        // }
+
+        window.google.books.load() 
+        window.google.books.setOnLoadCallback(initialize)
+
+
+        // setTimeout(() => {
+        //     window.google.books.load() }, 2000) 
+
+        //     setTimeout(() => {
+        //         window.google.books.setOnLoadCallback(initialize) }, 4000) 
+       
+
         
         function initialize() {
             var viewer = new window.google.books.DefaultViewer(canvasRef.current);
@@ -20,12 +39,12 @@ const BookViewer = () => {
             console.log(viewer)
           }
 
-        window.google.books.setOnLoadCallback(initialize);
 
        
     }, [])
 
-
+    // Ternary - if state is false (not loading), return canvas
+    //If loading is true, return gif
     return(
         <div>
             <div ref={canvasRef} id="viewerCanvas"></div>
