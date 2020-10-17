59c3a38434e69ef892067ef9d1c1a1d5d93b3102
chsohn15 <chsohn15@gmail.com>
2020-10-17 19:25:34 -0400

    add variables to tier two container

--- book-quest-react/src/components/rewardsPage/tiers/TierTwoContainer.js
+++ book-quest-react/src/components/rewardsPage/tiers/TierTwoContainer.js
@@ -1,6 +1,22 @@
 import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import { creatingReward } from '../../../redux/actions.js' 
+import { useSelector } from "react-redux";
 
 const TierTwoContainer = (props) => {
+
+    const student_id = useSelector(state => state.userReducer.currentUser.id)
+    // const errors = useSelector(state => state.errorsReducer.errors)
+
+    const level = 2
+    const buttons_price = 25
+    const buttons_description = "body"
+
+    const eyes_price = 20
+    const eyes_description = "face"
+
+    const nose_price = 20
+    const nose_description = "face"
 
     return(
         <React.Fragment>
