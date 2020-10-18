f44336ca165049aaafaf0367463d88f6545b3f6f
chsohn15 <chsohn15@gmail.com>
2020-10-18 13:46:16 -0400

    add drawer to home page

--- book-quest-react/src/components/SignUp2.js
+++ book-quest-react/src/components/SignUp2.js
@@ -66,8 +66,8 @@ function SignUp2(props) {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      {/* <Grid item xs={false} sm={4} md={7} className={classes.image} /> */}
+      <Grid item component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
           <MenuBookIcon />
--- book-quest-react/src/components/Welcome.js
+++ book-quest-react/src/components/Welcome.js
@@ -1,21 +1,88 @@
-import React from 'react'
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import SignUp from './SignUp2'
 
-const Welcome = (props) => {
+const useStyles = makeStyles({
+  list: {
+    width: 500,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
-    const goToSignUpPage = () => {
-        props.history.push("/signup");
-      };
+function Welcome() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+    right: false,
+  });
 
-      const goToLoginPage = () => {
-        props.history.push("/login");
-      };
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-    return(
-        <div id="welcome-container">
-            <button onClick={() => goToSignUpPage()}>Sign Up Today!</button>
-            <button onClick={() => goToLoginPage()}>Login</button>
-        </div>
-    )
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <SignUp />
+      
+    </div>
+  );
+
+  return (
+    <div id="welcome-container">
+      {['left', 'right'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>Sign Up</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
+
+    // const goToSignUpPage = () => {
+    //     props.history.push("/signup");
+    //   };
+
+    //   const goToLoginPage = () => {
+    //     props.history.push("/login");
+    //   };
+
+    // return(
+    //     <div id="welcome-container">
+    //         <button onClick={() => goToSignUpPage()}>Sign Up Today!</button>
+    //         <button onClick={() => goToLoginPage()}>Login</button>
+    //     </div>
+    // )
+
 
 export default Welcome
