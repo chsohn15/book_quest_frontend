395ff58642fc912ae228d469fa0af118742fabee
chsohn15 <chsohn15@gmail.com>
2020-10-19 18:41:44 -0400

    fixed embedded viewer

--- book-quest-react/src/components/userHome/books/BookViewer.js
+++ book-quest-react/src/components/userHome/books/BookViewer.js
@@ -8,6 +8,7 @@ import { useSelector } from "react-redux";
 const BookViewer = () => {
     const ISBN_num = useSelector(state => state.currentBookReducer.currentBook.book.ISBN_number)
     const canvasRef = useRef()
+    
 
     const [loaded, setLoaded] = useState(false);
     //const [pageReloaded, setPageReloaded] = useState(false);
@@ -20,12 +21,12 @@ const BookViewer = () => {
       scriptTag.id = "google-script"
       document.body.appendChild(scriptTag);
 
-      
-      return(
-        () => {
-        document.getElementById('google-script').remove()
-        }
-      )
+  
+      // return(
+      //   () => {
+      //   document.getElementById('google-script').remove()
+      //   }
+      // )
     }, []);
   
     useEffect(()=>{
@@ -33,36 +34,37 @@ const BookViewer = () => {
       if (!loaded) return
         // { setTimeout( () => {window.location.reload(true);}, 2000 )}
       else{
-        console.log("IN ELSE")
-        window.google.books.load() 
+
+        if(window.viewer){
+          //debugger
+          let viewer = new window.google.books.DefaultViewer(canvasRef.current);
+          
+          viewer.load('ISBN:'+ ISBN_num, alertNotFound);
+
+          function alertNotFound() {
+            alert("could not embed the book!");
+          }
+          
+        }
+        else{
+          window.google.books.load() 
           
         console.log(window.google.books.setOnLoadCallback)
-        if(window.google.books.setOnLoadCallback() === undefined){
-          
-          //setTimeout( () => {window.location.reload()}, 2000)
-          //return false;
-        }
+
 
         window.google.books.setOnLoadCallback(() => {
           console.log("LOADED")
-          var viewer = new window.google.books.DefaultViewer(canvasRef.current);
+          let viewer = new window.google.books.DefaultViewer(canvasRef.current);
           window.viewer = viewer
-          viewer.load('ISBN:'+ ISBN_num, alertNotFound, alertInitialized);
+          viewer.load('ISBN:'+ ISBN_num, alertNotFound);
+
+          function alertNotFound() {
+            //Change state
+            alert("could not embed the book!");
+          }
         })
-    
-    
-        function alertNotFound() {
-          alert("This Book Was Not Found on Google Books!");
-        }
 
-        function alertInitialized() {
-          alert("book successfully loaded and initialized!");
-        }
 
-        function initialize() {
-            var viewer = new window.google.books.DefaultViewer(canvasRef.current);
-            viewer.load('ISBN:'+ ISBN_num);
-            
         }
           
     }}, [loaded])
@@ -81,89 +83,4 @@ const BookViewer = () => {
 
 
 export default BookViewer
-
-// const BookViewer = () => {
-//     const ISBN_num = useSelector(state => state.currentBookReducer.currentBook.book.ISBN_number)
-//     const canvasRef = useRef()
-
-//      useEffect(() => {
-//         console.log(canvasRef)
-
-//         //if(!window.google) return;
-
-//         window.google.books.load() 
-//         window.google.books.setOnLoadCallback(initialize)
-       
-
-//         function initialize() {
-//             var viewer = new window.google.books.DefaultViewer(canvasRef.current);
-//             viewer.load('ISBN:'+ISBN_num);
-//             console.log(viewer)
-//           }
-    
-// })
-
-//     return(
-//         <div>
-//             <div ref={canvasRef} id="viewerCanvas"></div>
-//         </div>
-//     )
-// }
-
-
-
-
-
-    // const loadBook = () => {
-    //         window.google.books.load() 
-    //         window.google.books.setOnLoadCallback(initialize)
-
-    //         function initialize() {
-    //             var viewer = new window.google.books.DefaultViewer(canvasRef.current);
-    //             viewer.load('ISBN:'+ISBN_num);
-    //             console.log(viewer)
-    // }}
-    // useEffect(() => {
-    //     console.log(canvasRef)
-
-    //     //Check if google.books.load is a function
-
-    //     // if (window.google.books){
-    //     // window.google.books.load() 
-    //     // window.google.books.setOnLoadCallback(initialize)
-    //     // }
-
-    //     if(!window.google) return;
-
-    //     window.google.books.load() 
-    //     window.google.books.setOnLoadCallback(initialize)
-        
-
-
-    //     // setTimeout(() => {
-    //     //     window.google.books.load() }, 2000) 
-
-    //     //     setTimeout(() => {
-    //     //         window.google.books.setOnLoadCallback(initialize) }, 4000) 
-       
-
-        
-    //     function initialize() {
-    //         var viewer = new window.google.books.DefaultViewer(canvasRef.current);
-    //         viewer.load('ISBN:'+ISBN_num);
-    //         console.log(viewer)
-    //       }
-
-
-       
-    // }, [])
-
-    // Ternary - if state is false (not loading), return canvas
-    //If loading is true, return gif
-    // return(
-    //     <div>
-    //         <div ref={canvasRef} id="viewerCanvas"></div>
-    //     </div>
-    // )
-
 
