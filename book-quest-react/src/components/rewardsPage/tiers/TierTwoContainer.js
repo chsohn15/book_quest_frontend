ad093b4a005417c6ce193c77d04aca47d49d985e
chsohn15 <chsohn15@gmail.com>
2020-10-19 13:07:27 -0400

    add all cards for tier two container

--- book-quest-react/src/components/rewardsPage/tiers/TierTwoContainer.js
+++ book-quest-react/src/components/rewardsPage/tiers/TierTwoContainer.js
@@ -40,7 +40,7 @@ const TierTwoContainer = (props) => {
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image="https://thumbs.dreamstime.com/b/child-making-snowball-isolated-vector-illustration-graphic-cute-child-making-snowball-isolated-vector-illustration-graphic-130272751.jpg"
+                    image="https://www.thewonderofchristmas.com/wp-content/uploads/2009/08/children-building-snowman.jpg"
                     title="Building Snowman Buttons"
                     />
                 <CardContent>
@@ -67,6 +67,7 @@ const TierTwoContainer = (props) => {
             }
         </CardActions>
     </Card>
+
     <Card className={classes.root}>
             <CardActionArea>
                 <CardMedia
@@ -103,7 +104,7 @@ const TierTwoContainer = (props) => {
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSblHD2y_qkduDEOFEmSUb7SnAODb-Q-_A4vA&usqp=CAU"
+                    image="https://st4.depositphotos.com/6464944/30940/v/950/depositphotos_309403198-stock-illustration-traditional-winter-time-activity-man.jpg"
                     title="Building Snowman Nose"
                     />
                 <CardContent>
@@ -130,10 +131,42 @@ const TierTwoContainer = (props) => {
             }
         </CardActions>
     </Card>
+
+    <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image="https://lh3.googleusercontent.com/proxy/kDX2k_rNwPRn6hsCc6Ygfld-kRq6y_07l4XN6L0RVT8buiQc-JJYq6RC6vx1vNWi25i6gAN8yNx0Woyi85M0VgnFCdsix1BtgGary3F-exxU"
+                    title="Building Snowman Arms"
+                    />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Arms
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Add arms to your snowman!
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Price: 20 Stars
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        <CardActions>
+            {rewardsHash["arms"] ? 
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Redeemed!
+                </Typography> 
+                : 
+                <Button size="small" color="primary" onClick={() =>props.creatingReward(arms_price, level, student_id, arms_description)}>
+                Redeem Reward
+                </Button>
+            }
+        </CardActions>
+    </Card>
             {/* <button onClick={() => props.creatingReward(buttons_price, level, student_id, buttons_description)}>Add Buttons</button> */}
             {/* <button onClick={() => props.creatingReward(eyes_price, level, student_id, eyes_description)}>Add Eyes</button> */}
-            <button onClick={() => props.creatingReward(nose_price, level, student_id, nose_description)}>Add a Carrot Nose</button>
-            <button onClick={() => props.creatingReward(arms_price, level, student_id, arms_description)}>Add Arms</button>
+            {/* <button onClick={() => props.creatingReward(nose_price, level, student_id, nose_description)}>Add a Carrot Nose</button> */}
+            {/* <button onClick={() => props.creatingReward(arms_price, level, student_id, arms_description)}>Add Arms</button> */}
         </React.Fragment>
     )
 }
