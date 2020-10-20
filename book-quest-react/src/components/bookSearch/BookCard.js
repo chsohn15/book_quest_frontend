77818fa6244cfcb93f344166da9ee202e77ea6e3
chsohn15 <chsohn15@gmail.com>
2020-10-20 15:55:23 -0400

    add alert to book search

--- book-quest-react/src/components/bookSearch/BookCard.js
+++ book-quest-react/src/components/bookSearch/BookCard.js
@@ -3,12 +3,26 @@ import { connect } from 'react-redux'
 //import { useState } from 'react'
 import { addingBook } from '../../redux/actions.js'
 import { NavLink } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const BookCard = (props) => {
     const {title, authorsArray, publisher, publishedDate, description, pageCount, image_url, ISBN} = props
     
     const first_author = authorsArray[0]
 
+    const handleClick = (title, first_author, ISBN, image_url, pageCount) => {
+        props.addingBook(title, first_author, ISBN, image_url, pageCount)
+
+        Swal.fire({
+            text: `You've added ${title} to your bookshelf!`,
+            imageUrl: image_url,
+            imageWidth: 200,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+          })
+    }
+    
+    
     return(
         <div>
             <img src={image_url} alt="book"/>
@@ -20,7 +34,7 @@ const BookCard = (props) => {
             <div>Description: {description}</div>
             <div>Pages: {pageCount}</div>
             <NavLink to="user_home">
-                <button onClick={() => props.addingBook(title, first_author, ISBN, image_url, pageCount)}>Add to Book Shelf</button>
+                <button onClick={() => handleClick(title, first_author, ISBN, image_url, pageCount)}>Add to Book Shelf</button>
             </NavLink>
 
         </div>
